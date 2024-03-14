d4dcb06f8fbf4787e24286ddfc4917d606fb449e
Hira Zahid <47859592+HiraZahid98@users.noreply.github.com>
2024-03-14 18:05:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 
 const corsOptions = {
   //   origin: "https://mplayer1.netlify.app",
-  origin: "https://mp4converter.netlify.app/", //your frontend url here
-// origin:"null",
+  // origin: "https://mp4converter.netlify.app/", //your frontend url here
+origin:"null",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
   exposedHeaders: "**",
